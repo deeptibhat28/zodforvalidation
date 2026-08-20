f74558eee9d97d18b933fb6ab459495c3bc3230e
deeptibhat28 <138190658+deeptibhat28@users.noreply.github.com>
2026-08-20 16:51:56 +0530

    Fix footer

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -12,9 +12,9 @@ import messages from "@/messages.json"
 const Home = () => {
   return (
     <>
-    <main className="flex min-h-[80vh] flex-col items-center justify-center gap-6 py-6 px-6">
+    <main className="flex min-h-[80vh] flex-col items-center justify-center gap-4 py-4 px-6">
       <section className='text-center mb-8 md:mb-12'>
-        <h1 className='text-3xl md:text-5xl font-bold' >Dive into the mystery world of Anonymous Conversations</h1>
+        <h1 className='text-2xl md:text-4xl font-bold' >Dive into the mystery world of Anonymous Conversations</h1>
         <p className='mt-3 md:mt-4 text-base'>Explore Mystery Message - Where your identity remains a secret.</p>
       </section>
        <Carousel plugins={[Autoplay({delay: 2000})]} className="w-full max-w-xs">
@@ -25,7 +25,7 @@ const Home = () => {
             <div className="p-1">
               <Card>
                 <CardHeader>{message.title}</CardHeader>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
+                <CardContent className="flex aspect-square items-center justify-center p-4">
                   <span className="text-lg font-semibold">
                     {message.content}
                   </span>
@@ -40,7 +40,7 @@ const Home = () => {
       <CarouselNext />
     </Carousel>
     </main>
-    <footer className="text-center p-4 md:p-6">© 2026 Mystery Message. All rights reserved.</footer>
+    <footer className="w-full text-center py-4 bg-transparent text-gray-700 text-sm mt-auto">© 2026 Mystery Message. All rights reserved.</footer>
     </>
   )
 }
